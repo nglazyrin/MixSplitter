7a901788e12bad49f2c7b8aff06db5cec4b993a7
nglazyrin <nglazyrin@gmail.com>
2015-04-30 13:03:21 +0500

    Use mir_eval for evaluation. Code cleanup.

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ __author__ = 'Nikolay'
 
 import cPickle
 import csv
+from mir_eval.segment import validate_structure, pairwise, deviation
 import numpy
 import os
 import subprocess
@@ -9,9 +10,11 @@ import tools
 import cProfile
 
 
-plugin_suffix = '_vamp_qm-vamp-plugins_qm-constantq_constantq'
-vamp_path = 'sonic-annotator-1.0-win32/sonic-annotator.exe'
+vamp_path = 'sonic-annotator-1.1-win32/sonic-annotator.exe'
 n3_path = 'config/constantq_hires.n3'
+plugin_suffix = '_vamp_qm-vamp-plugins_qm-constantq_constantq'
+# n3_path = 'config/mfcc.n3'
+# plugin_suffix = '_vamp_vamp-libxtract_mfcc_mfcc'
 
 
 def read_csv(filename):
@@ -48,15 +51,13 @@ def read_cue(filename):
                 current['PERFORMER'] = line.split('"')[1]
             if line.startswith('TITLE'):
                 current['TITLE'] = line.split('"')[1]
-    # if 'intro' in info[0]['TITLE'].lower():
-    #     info = info[1:]
-    #     info[0]['INDEX'] = 0
     return info
 
 
 def extract_features(mp3):
     mp3_path = os.path.abspath(mp3).replace('\\', '/')
-    return_code = subprocess.call(vamp_path + ' -t ' + n3_path + ' "' + mp3_path + '" -w csv')
+    cmd = vamp_path + ' -t ' + n3_path + ' "' + mp3_path + '" -w csv'
+    return_code = subprocess.call(cmd)
     if return_code != 0:
         print 'Feature extraction process exited with return code %d, skipping the %s' % (return_code, mp3)
 
@@ -83,12 +84,22 @@ def print_results(results, log):
     print '%d mixes have been processed' % (len(results))
     log.write('%d mixes have been processed\r\n' % (len(results)))
     avg = 0
+    avgmax = 0
     for result in results:
         avg += result['avg_diff']
+        avgmax += result['max_diff']
     print 'Average difference from CUE file: %f' % (avg / len(results))
     log.write('Average difference from CUE file: %f\r\n' % (avg / len(results)))
+    print 'Average max difference from CUE file: %f' % (avgmax / len(results))
+    log.write('Average max difference from CUE file: %f\r\n' % (avgmax / len(results)))
     # for result in results:
     #     print '\t%s: avg_diff: %f, max_diff: %f' % (result['name'], result['avg_diff'], result['max_diff'])
+
+
+def create_labeled_intervals(borders):
+    intervals = [(borders[i], borders[i+1]) for i in range(len(borders) - 1)]
+    labels = [str(i+1) for i in range(len(intervals))]
+    return numpy.array(intervals), labels
 
 
 def main():
@@ -98,7 +109,6 @@ def main():
     nc = tools.NoveltyCalculator()
     with open(os.path.join('logs', 'test.log'), 'wb') as log:
         for mp3 in mp3_files:
-            #mp3 = mp3_files[0]
             name = mp3[:-4]
             cue = os.path.join(data_dir, name + '.cue')
             info = None
@@ -113,7 +123,7 @@ def main():
                 #has_outro = False
                 csv_file = os.path.join(data_dir, name + plugin_suffix + '.csv')
                 npz_file = os.path.join(data_dir, name + plugin_suffix + '.npz')
-                self_sim_file = os.path.join(data_dir, name + plugin_suffix + '.sim')
+                self_sim_file = os.path.join(data_dir, name + plugin_suffix + '.sim.npz')
                 if not os.path.isfile(csv_file) and not os.path.isfile(npz_file):
                     extract_features(os.path.join(data_dir, mp3))
                 if os.path.isfile(npz_file):
@@ -127,9 +137,7 @@ def main():
                     if os.path.isfile(self_sim_file):
                         saved = numpy.load(self_sim_file)
                         self_sim = saved['self_sim']
-                        factor = saved['factor'][0]
-                        # with open(self_sim_file, 'rb') as f:
-                            # (self_sim, factor) = cPickle.load(f)
+                        factor = saved['factor']
                         borders = tools.detect_track_borders(data,
                                                              timestamps[-1],
                                                              len(info),
@@ -146,10 +154,24 @@ def main():
                                                              sim_file=self_sim_file,
                                                              has_intro=has_intro,
                                                              has_outro=has_outro)
+                    if len(borders) < len(info):
+                        print len(borders), len(info)
+
+                    true_borders = [c['INDEX'] for c in info]
+                    true_borders.append(borders[-1])
+                    expected_intervals, expected_labels = create_labeled_intervals(true_borders)
+                    actual_intervals, actual_labels = create_labeled_intervals(borders)
+                    validate_structure(expected_intervals, expected_labels, actual_intervals, actual_labels)
+                    precision, recall, f_measure = \
+                        pairwise(expected_intervals, expected_labels, actual_intervals, actual_labels, frame_size=1)
+                    actual_to_expected, expected_to_actual = deviation(expected_intervals, actual_intervals)
                     avg_diff, max_diff = get_diff(info, borders)
                     result.append({'name': mp3, 'info': info, 'borders': borders, 'avg_diff': avg_diff, 'max_diff': max_diff})
-                    print '%s\t%f\t%f' % (mp3, avg_diff, max_diff)
-                    log.write('%s\t%f\t%f\t%s\r\n' % (mp3, avg_diff, max_diff, borders))
+                    print '%s\t%.3f\t%.3f\t\t%.4f\t%.4f\t%.4f\t\t%.4f\t%.4f' % \
+                          (mp3, avg_diff, max_diff, precision, recall, f_measure, actual_to_expected, expected_to_actual)
+                    log.write('%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%s\r\n' %
+                              (mp3, avg_diff, max_diff, precision, recall, f_measure, actual_to_expected,
+                               expected_to_actual, borders))
         print_results(sorted(result, key=lambda x: x['name']), log)
 
 
